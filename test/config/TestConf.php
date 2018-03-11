1ca63fe678cb3ec3c3a043551b83886de27e8666
Unknown <at.frizzy@gmail.com>
2018-03-11 17:22:03 +0100

    testConf (test phpunit)

--- test/config/TestConf.php
+++ test/config/TestConf.php
@@ -1,8 +1,40 @@
 <?php
 
+require_once "../../phpunit.phar";
+require_once "../../config/Conf.php";
+
 use PHPUnit\Framework\TestCase;
 
 class TestConf extends TestCase
 {
+    // Test passed
+    function testGetDebug()
+    {
+        $this->assertTrue(Conf::getDebug());
+    }
 
+    // Test passed
+    function testGetLogin()
+    {
+        $this->assertEquals("phunvongm", Conf::getLogin());
+    }
+
+    // Test passed
+    function testGetHostname()
+    {
+        $this->assertEquals("webinfo", Conf::getHostname());
+    }
+
+    // Test passed
+    function testGetDatabase()
+    {
+        $this->assertEquals("phunvongm", Conf::getDatabase());
+    }
+
+    // Test passed
+    function testGetPassword()
+    {
+        $this->assertEquals("Nani?!", Conf::getPassword());
+    }
+    
 }
