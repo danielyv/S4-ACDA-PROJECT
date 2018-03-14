9fc0c51221e1dd2fe92f8f40f69b2c8c40c23aad
Unknown <at.frizzy@gmail.com>
2018-03-14 18:44:15 +0100

    test modelutilisateur

--- test/model/TestModelUtilisateur.php
+++ test/model/TestModelUtilisateur.php
@@ -2,7 +2,60 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once '../../lib/File.php';
+require_once '../../lib/Security.php';
+require_once '../../model/Model.php';
+require_once '../../model/ModelUtilisateur.php';
+
 class TestModelUtilisateur extends TestCase
 {
 
+    function testCheckPassword()
+    {
+        $this->assertTrue(ModelUtilisateur::checkPassword("evilox",Security::chiffrer("oui")));
+        $this->assertFalse(ModelUtilisateur::checkPassword("evilox",Security::chiffrer("ouzi")));
+    }
+
+    function testGetNameByLogin()
+    {
+        $name = ModelUtilisateur::getNameByLogin("evilox");
+        $this->assertEquals("password", $name);
+    }
+
+    function testGetNameByLogin1() {
+        $name = ModelUtilisateur::getNameByLogin("eviloxqs");
+        $this->assertFalse($name);
+    }
+
+    function testGetter()
+    {
+        $test = ModelUtilisateur::select("evilox");
+        $this->assertEquals("evilox",$test->get('login'));
+        $this->assertEquals("cd0eb7716aab9d4e4910b50a4438264030aacb9474661ed7639472208e985d75",$test->get('mdp'));
+        $this->assertEquals("password",$test->get('pseudo'));
+        $this->assertEquals("ev@yopmail.com",$test->get('email'));
+        $this->assertEquals("0",$test->get('admin'));
+        $this->assertEquals(null,$test->get('nonce'));
+        $this->assertEquals("zegf",$test->get('sexe'));
+        $this->assertEquals("ef",$test->get('profession'));
+    }
+
+    function testSetter()
+    {
+        $test = new ModelUtilisateur();
+        $properties = array(
+            "login",
+            "mdp",
+            "pseudo",
+            "email",
+            "nonce",
+            "admin",
+            "sexe",
+            "profession"
+        );
+        foreach ($properties as $property) {
+            $test->set($property,"1");
+            $this->assertEquals("1",$test->get($property));
+        }
+    }
 }
